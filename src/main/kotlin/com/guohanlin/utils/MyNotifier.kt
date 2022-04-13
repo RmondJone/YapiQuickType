37939881c58a8815570201b9d0d5c237285bb418
guohanlin <2318560278@qq.com>
2022-04-13 10:38:30 +0800

    [Bug修复]: 兼容2020.3以下版本

--- src/main/kotlin/com/guohanlin/utils/MyNotifier.kt
+++ src/main/kotlin/com/guohanlin/utils/MyNotifier.kt
@@ -1,7 +1,10 @@
 package com.guohanlin.utils
 
+import com.intellij.notification.NotificationDisplayType
+import com.intellij.notification.NotificationGroup
 import com.intellij.notification.NotificationGroupManager
 import com.intellij.notification.NotificationType
+import com.intellij.openapi.application.ApplicationInfo
 import com.intellij.openapi.project.Project
 import java.util.*
 import kotlin.concurrent.schedule
@@ -19,10 +22,17 @@ object MyNotifier {
      */
     @JvmStatic
     fun notifyError(project: Project, content: String) {
-        NotificationGroupManager.getInstance()
-            .getNotificationGroup("YApi QuickType Error")
-            .createNotification(content, NotificationType.ERROR)
-            .notify(project)
+        if (getIDEVersion() > 203) {
+            NotificationGroupManager.getInstance()
+                .getNotificationGroup("YApi QuickType Error")
+                .createNotification(content, NotificationType.ERROR)
+                .notify(project)
+        } else {
+            //2020.3版本以下使用这种方式通知
+            NotificationGroup("YApi QuickType Error", NotificationDisplayType.BALLOON, true)
+                .createNotification(content, NotificationType.ERROR)
+                .notify(project)
+        }
     }
 
     /**
@@ -32,13 +42,28 @@ object MyNotifier {
      */
     @JvmStatic
     fun notifyMessage(project: Project, content: String) {
-        val notification = NotificationGroupManager.getInstance()
-            .getNotificationGroup("YApi QuickType Message")
-            .createNotification(content, NotificationType.INFORMATION)
+        val notification = if (getIDEVersion() > 203) {
+            NotificationGroupManager.getInstance()
+                .getNotificationGroup("YApi QuickType Message")
+                .createNotification(content, NotificationType.INFORMATION)
+        } else {
+            //2020.3版本以下使用这种方式通知
+            NotificationGroup("YApi QuickType Message", NotificationDisplayType.BALLOON, true)
+                .createNotification(content, NotificationType.INFORMATION)
+        }
         notification.notify(project)
         //2s之后消失
         Timer().schedule(2000) {
             notification.expire()
         }
+    }
+
+    /**
+     * 注释：获取IDE版本号
+     * 时间：2022/4/13 6:33 下午
+     * 作者：郭翰林
+     */
+    private fun getIDEVersion(): Int {
+        return ApplicationInfo.getInstance().build.components[0]
     }
 }
