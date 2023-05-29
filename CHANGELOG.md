2fda67e102fa8517b617835915e148eb7e696d46
RmondJone <2318560278@qq.com>
2023-05-29 19:58:25 +0800

    feat: 修复Java更新文件不对问题

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,5 @@
-1、修复Https不能正常请求问题
+1、修复Java更新文件生成不对问题
 <br/>
 <br/>
 
-1. Fix the issue of Https not requesting properly
+1. Fix Java update file generation issue
--- src/main/kotlin/com/guohanlin/language/android/JavaModelCodeStructure.kt
+++ src/main/kotlin/com/guohanlin/language/android/JavaModelCodeStructure.kt
@@ -22,32 +22,38 @@ class JavaModelCodeStructure(
     CodeStructure(directory, data) {
 
     override fun creatCode(): String {
-        var code: String = interfaceResponseDTO.info
-        code = code.replaceFirst(
-            "package io.quicktype;",
-            "package ${getPackageName()};\nimport java.util.List;"
-        )
-        code = code.replace("package io.quicktype;", "")
-        //删除多余的public class
-        code = code.replace("public class", "class")
-        code = code.replaceFirst("class", "public class")
-        //删除多余的import java.util.List;
-        code = code.replace("import java.util.List;", "import ListPackage")
-        code = code.replaceFirst("import ListPackage", "import java.util.List;")
-        code = code.replace("import ListPackage", "")
-        //删除多余空格
-        code = code.replace("\n\n\n", "\n")
-        return code
+        return conversion(interfaceResponseDTO.info)
     }
 
     override fun updateCode(psiFile: PsiFile) {
         val document = PsiDocumentManager.getInstance(getProject()).getDocument(psiFile)
         document?.deleteString(0, document.textLength)
-        var code: String = interfaceResponseDTO.info
-        code = code.replace("package io.quicktype;", "package ${getPackageName()};")
-        //删除多余空格
-        code = code.replace("\n\n\n", "\n")
+        val code: String = conversion(interfaceResponseDTO.info)
         document?.insertString(0, code)
+    }
+
+    /**
+     * 注释：转换Code
+     * 时间：2023/5/29 19:55
+     * 作者：郭翰林
+     */
+    private fun conversion(code: String): String {
+        var result = code
+        result = result.replaceFirst(
+            "package io.quicktype;",
+            "package ${getPackageName()};\nimport java.util.List;"
+        )
+        result = result.replace("package io.quicktype;", "")
+        //删除多余的public class
+        result = result.replace("public class", "class")
+        result = result.replaceFirst("class", "public class")
+        //删除多余的import java.util.List;
+        result = result.replace("import java.util.List;", "import ListPackage")
+        result = result.replaceFirst("import ListPackage", "import java.util.List;")
+        result = result.replace("import ListPackage", "")
+        //删除多余空格
+        result = result.replace("\n\n\n", "\n")
+        return result
     }
 
     override fun creatFileName(): String {
